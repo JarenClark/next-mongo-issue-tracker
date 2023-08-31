88ba58a8038088636a92be624d9566e24a20da52
JarenClark <jarenclark1@gmail.com>
2023-08-30 21:42:20 -0700

    added UI lib / new models

---
+++ models/notification.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const notificationSchema = new Schema({
+  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user receiving the notification
+  content: { type: String, required: true }, // Notification message or content
+  type: { type: String, required: true }, // Type of notification (e.g., task_assigned, comment_added, due_date_reminder)
+  is_read: { type: Boolean, default: false }, // Whether the user has read the notification
+  created_at: { type: Date, default: Date.now }, // Timestamp when the notification was created
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
--- redux/reducers/userReducer.js
+++ redux/reducers/userReducer.js
@@ -5,3 +5,28 @@ import {
     LOGIN_USER_FAIL,
     CLEAR_ERRORS
 } from '../constants/userConstants'
+
+export const userReducer = (state = { user: {}}, actions) => {
+    switch (actions.type) {
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state;
+    }
+}
+
+
+export const allUsersReducer = (state = { users: [] }, actions) => {
+    switch (actions.type) {
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state;
+    }
+}
